f9ce0f5b5f87443a86f84bba662a5cabff43d680
Mochatr <mohamedchatr8@gmail.com>
2023-11-20 06:58:20 -0800

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,6 +5,7 @@
  * @array: An array
  * @size: The size of the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
 	int swap;
@@ -18,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < (size - 1); j++)
 		{
-			if (array[k] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				swap = array[j];
 				array[j] = array[j + 1];
