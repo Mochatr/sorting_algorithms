b4a7f3fe8db2965693d5ec82c34d6be247c1f3f2
Mochatr <mohamedchatr8@gmail.com>
2023-11-20 06:48:31 -0800

    Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers
+ * @array: An array
+ * @size: The size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swap;
+	size_t i = 0;
+	size_t j;
+
+	if (!array || !size)
+		return;
+
+	while (i < size)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[k] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
+
